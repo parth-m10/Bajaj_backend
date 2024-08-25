85ea4aacbfbfaadcb629da2b0ff4665bbd39a2fa
parth-m10 <136921141+parth-m10@users.noreply.github.com>
2024-08-25 12:50:58 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const cors = require('cors');
+app.use(cors({
+    origin: 'https://bajaj-frontend-6z55.onrender.com', // Replace with your frontend URL
+    methods: ['GET', 'POST'],
+}));
+
+app.post('/bfhl', (req, res) => {
+    const { data } = req.body;
+    const user_id = "Parth Jariwala";  
+    const email = "parth10.jariwala@gmail.com";  
+    const roll_number = "21BAI1276"; 
+
+    const numbers = data.filter(item => !isNaN(item));
+    const alphabets = data.filter(item => isNaN(item));
+
+    const lowercaseAlphabets = alphabets.filter(char => char === char.toLowerCase());
+    const highest_lowercase_alphabet = lowercaseAlphabets.length ? [lowercaseAlphabets.sort().pop()] : [];
+
+    res.json({
+        is_success: true,
+        user_id,
+        email,
+        roll_number,
+        numbers,
+        alphabets,
+        highest_lowercase_alphabet
+    });
+});
+
+app.get('/bfhl', (req, res) => {
+    res.status(200).json({ operation_code: 1 });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
